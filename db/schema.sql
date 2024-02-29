3a58ed4d0e6e096d8ccec91ab5160f9283e37dbf
Armond-Guze <Armond.Guze@yahoo.com>
2024-02-28 21:18:23 -0500

    created schema/ tables and database

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,30 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+CREATE TABLE Category {
+    ID INT NOT NULL AUTO_INCREMENT,
+    Category_name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id)
+};
+
+CREATE TABLE Product (
+    ID INT NOT NULL AUTO_INCREMENT,
+    Product_name VARCHAR(45) NOT NULL,
+    Price DECIMAL(15,5) NOT NULL,
+    Stock INT(10) NOT NULL,
+    Category_id INT,
+    FOREIGN KEY (Category_id) REFERENCES Category(id),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Tag (
+    ID INT NOT NULL AUTO_INCREMENT,
+    Product_id INT,
+    FOREIGNKEY(Product_id) REFERENCES Product(id),
+    Tag_id INT,
+    FOREIGN KEY(Tag_id) REFERENCES Tag(id),
+    PRIMARY KEY(id)
+);
+
